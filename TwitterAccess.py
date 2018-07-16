53168ab44c48ac808f11dc869c8444e9db984c5d
Kodai Niwa <niwahkme@gmail.com>
2018-07-17 02:10:02 +0900

    APIキーを外部ファイルから読み込むよう変更

--- TwitterAccess.py
+++ TwitterAccess.py
@@ -7,13 +7,17 @@ from PIL import Image
 
 class TwitterAccess:
 
-    CONSUMER_KEY =''
-    CONSUMER_SECRET =''
-    ACCESS_TOKEN =''
-    ACCESS_SECRET =''
+    jsonKey = {}
 
     def __init__(self):
-        print('initialized')
+        basePath = os.path.dirname(os.path.abspath(__file__))
+        filePath = os.path.normpath(os.path.join(basePath, './twkey.json'))
+        
+        fileKey = open(filePath)
+        self.jsonKey = json.load(fileKey)
+        fileKey.close()
+        
+        print('TwitterAccess initialized')
 
     def tweet(self,text):
         apiInst = Twython(TwitterAccess.CONSUMER_KEY,TwitterAccess.CONSUMER_SECRET,TwitterAccess.ACCESS_TOKEN,TwitterAccess.ACCESS_SECRET)
