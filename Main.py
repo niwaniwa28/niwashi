dca65936fc55166162372f73af1eadbfa783dfc3
Kodai Niwa <niwahkme@gmail.com>
2018-07-16 22:26:11 +0900

    照明のON/OFFを追加

--- Main.py
+++ Main.py
@@ -5,6 +5,8 @@ import dht11
 import time
 import datetime
 import os
+import subprocess
+
 from twython import Twython
 
 import TwitterAccess
@@ -25,6 +27,9 @@ filePath = '/usr/app/niwashi/img/capture.jpg'
 twitter = TwitterAccess.TwitterAccess()
 capt = ImageCapture.ImageCapture()
 
+powerOnCmd = "sudo python /usr/app/hub_ctrl.py -h 0 -P 1 -p 1"
+powerOffCmd = "sudo python /usr/app/hub_ctrl.py -h 0 -P 1 -p 0"
+
 errorCnt = 0
 loopContinue = True
 
@@ -37,7 +42,7 @@ while loopContinue:
     if clockTimeM > "30":
         sleepTime = 1800
     
-    if clockTimeH == "08" or clockTimeH == "11" or clockTimeH == "14" or clockTimeH == "17":
+    if clockTimeH == "01" or clockTimeH == "08" or clockTimeH == "11" or clockTimeH == "14" or clockTimeH == "17":
     
         current_time = datetime.datetime.now().strftime(dateTimeFormat)
 
@@ -51,6 +56,10 @@ while loopContinue:
                 
                 print(filePath)
                 
+                resCmd = subprocess.call(powerOnCmd.split())
+                print(resCmd)
+                time.sleep(3)
+                
                 if capt.putImage(filePath):
                     twitter.tweetImage(textTweet, filePath)
                 else:
@@ -58,30 +67,34 @@ while loopContinue:
                 
                 print('success tweeted.')
                 
+                resCmd = subprocess.call(powerOffCmd.split())
+                print(resCmd)
+                time.sleep(1)
+
                 errorCnt = 0
                 loopRead = False
                 
             else:
                 print('dht11 error')
                 
-                if errorCnt > 29:
-                    twitter.tweet('ヨミトリ エラー ガ 30カイ ヲ コエマシタ. カンシ ヲ チュウシ シマス.')
+                if errorCnt > 49:
+                    twitter.tweet('ヨミトリ エラー ガ 50カイ ヲ コエマシタ. カンシ ヲ チュウシ シマス.')
                     
-                    print('error over 30. error tweeted.')
+                    print('error over 50. error tweeted.')
                     
                     loopRead = False
                     loopContinue = False
                 
-                elif errorCnt == 10:
+                elif errorCnt > 0 and errorCnt % 10 == 0:
                     print('error over 10. recreate dht11 instance.')
                     
                     errorCnt = errorCnt + 1
-                    time.sleep(30)
+                    time.sleep(10)
                     
                     dht11Inst = dht11.DHT11(pin=4)
                 else:
                     errorCnt = errorCnt + 1
-                    time.sleep(1)
+                    time.sleep(3)
 
     time.sleep(sleepTime)
 
